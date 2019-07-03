4565371781bf0ccd1576763076e6e15bf3b2aff9
Ryan Matthews <ryan.matthews.professional@gmail.com>
2019-07-03 04:13:10 -0400

    twilio functional

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const authRouter = require("./auth/register")
 const accountSid = process.env.TWILIOSECRET;
 const authToken = process.env.TWILIOTOKEN;
 const client = require('twilio')(accountSid, authToken);
-
+const bodyParser = require('body-parser')
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors())
-
+server.use(bodyParser.urlencoded({ extended: false }))
 const port = process.env.PORT || 8000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
@@ -156,7 +156,7 @@ server.post("/fieldTrips",  (req, res)  =>  {
                     .join("students", "studentFieldTripJoin.student_id", "students.id")
                     .join("guardians", "students.guardian_id", "guardians.id")
                     .join("fieldTrips", "studentFieldTripJoin.fieldTrip_id", "fieldTrips.id")
-                    .select("guardians.contact", {"studentName":"students.name"}, {"tripName":"fieldTrips.name"}, "fieldTrips.date", {"guardianName": "guardians.name"})
+                    .select("guardians.contact", {"guardian_id": "guardians.id"}, {"guardianName": "guardians.name"},{"studentName":"students.name"},{"student_id": "students.id"}, {"tripName":"fieldTrips.name"}, "fieldTrips.date", {"fieldTrip_id": "fieldtrips.id"})
                     .where({fieldTrip_id: id[0]})
                     .then(data  =>  {
                         data.forEach(entry  =>  {
@@ -167,6 +167,14 @@ server.post("/fieldTrips",  (req, res)  =>  {
                                     to: process.env.TONUMBER
                                 })
                                 .then(message => console.log(message.sid));
+                                db("smsOutgoing")
+                                    .insert({fieldTrip_id: entry.fieldTrip_id, student_id: entry.student_id, guardian_id: entry.guardian_id})
+                                    .then(data  =>  {
+                                        console.log(data)
+                                    })
+                                    .catch(err  =>  {
+                                        res.status(500).json({error: err.message})
+                                    })
                         })
                         res.status(200).json(data)
                     })
@@ -238,7 +246,42 @@ server.delete("/fieldTrips/:id",    (req, res)  =>  {
 })
 
 server.post("/sms", (req, res)  =>  {
-    console.log("req", req)
-    console.log("req.body", req.body)
-    console.log("req.params", req.params)
+    const contact = req.body.From.slice(2)
+    console.log(contact)
+    console.log(req.body)
+    var today = new Date();
+    var dd = today.getDate();
+
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    if(dd<10)
+    {
+        dd='0'+dd;
+    }
+    if(mm<10)
+    {
+        mm='0'+mm;
+    }
+
+    today = yyyy+'-'+mm+'-'+dd;
+
+    db("guardians")
+        .where({contact})
+        .first()
+        .then(data  =>  {
+            console.log("guardian id", data.id)
+            db("smsOutgoing")
+                .where({guardian_id: data.id})
+                .first()
+                .then(data  =>  {
+                    console.log(data)
+                    db("studentFieldTripJoin")
+                    .where({fieldTrip_id: data.fieldTrip_id, student_id: data.student_id})
+                    .update({ status: 1})
+                    .then(data  =>  {
+                        console.log(data)
+                    })
+                })
+        })
+
 })
